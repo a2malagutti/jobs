8e88117679068b0c4e76c4eda2aa8a146619282a
a2malagutti <amalagutti@gmail.com>
2026-06-03 11:16:16 -0300

    2026-06-26

---
+++ icons/script.sh
@@ -0,0 +1,35 @@
+cp "especialista_gerente produtos.png"               especialista_ai_data_machine_learning.png
+cp "especialista_gerente projetos.png"               especialista_software_engineering_development.png
+cp "especialista_gerente produtos.png"               especialista_cloud_infrastructure_devops_security.png
+cp "especialista_gerente projetos.png"               especialista_architecture_engineering_career_ladders.png
+cp "especialista_gerente produtos.png"               especialista_management_agility_product_leadership.png
+cp "especialista_gerente projetos.png"               especialista_business_support_miscellaneous.png
+                                  
+cp "junior_gerente produtos.png"         junior_ai_data_machine_learning.png
+cp "junior_gerente projetos.png"         junior_software_engineering_development.png
+cp "junior_gerente produtos.png"         junior_cloud_infrastructure_devops_security.png
+cp "junior_gerente projetos.png"         junior_architecture_engineering_career_ladders.png
+cp "junior_gerente produtos.png"         junior_management_agility_product_leadership.png
+cp "junior_gerente projetos.png"         junior_business_support_miscellaneous.png
+     
+cp "outro_gerente produtos.png"         outro_ai_data_machine_learning.png
+cp "outro_gerente projetos.png"         outro_software_engineering_development.png
+cp "outro_gerente produtos.png"         outro_cloud_infrastructure_devops_security.png
+cp "outro_gerente projetos.png"         outro_architecture_engineering_career_ladders.png
+cp "outro_gerente produtos.png"         outro_management_agility_product_leadership.png
+cp "outro_gerente projetos.png"         outro_business_support_miscellaneous.png
+                 
+cp "pleno_gerente produtos.png"         pleno_ai_data_machine_learning.png
+cp "pleno_gerente projetos.png"         pleno_software_engineering_development.png
+cp "pleno_gerente produtos.png"         pleno_cloud_infrastructure_devops_security.png
+cp "pleno_gerente projetos.png"         pleno_architecture_engineering_career_ladders.png
+cp "pleno_gerente produtos.png"         pleno_management_agility_product_leadership.png
+cp "pleno_gerente projetos.png"         pleno_business_support_miscellaneous.png
+                    
+cp "senior_gerente produtos.png"         senior_ai_data_machine_learning.png
+cp "senior_gerente projetos.png"         senior_software_engineering_development.png
+cp "senior_gerente produtos.png"         senior_cloud_infrastructure_devops_security.png
+cp "senior_gerente projetos.png"         senior_architecture_engineering_career_ladders.png
+cp "senior_gerente produtos.png"         senior_management_agility_product_leadership.png
+cp "senior_gerente projetos.png"         senior_business_support_miscellaneous.png
+
